e8f531892c0b913e83735a77d8a8acc58e2c99c3
Alejandro <ale64bit@gmail.com>
2019-02-10 17:18:43 -0500

    Remove legacy implementation

--- legacy/ast.ml
+++
@@ -1,6 +0,0 @@
-type root = Root of {pipelines: pipeline list; renderers: renderer list}
-
-and pipeline = Pipeline of string list
-
-and renderer = Renderer of string list
-
--- test/util.ml
+++
@@ -1,9 +0,0 @@
-let rec drain stream = match Stream.peek stream with
-  | None -> []
-  | Some _ -> 
-      let t = Stream.next stream in
-      t :: (drain stream)
-
-let string_of_token_stream l = 
-  "[" ^ (String.concat ";" (List.map Rpdl.Token.to_string l)) ^ "]"
-
